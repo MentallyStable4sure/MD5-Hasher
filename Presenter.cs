b7d4c11eb914277e297d1c0589864543561c2e25
dental misorder <denys.bondarenko@nure.ua>
2023-12-15 07:49:44 +0200

    Presenter to show off our menus 🎁

---
+++ Presenter.cs
@@ -0,0 +1,27 @@
+﻿using MS4S_MD5Hasher.Data;
+
+namespace MS4S_MD5Hasher
+{
+
+    public partial class Presenter
+    {
+        private UIItem[] datas;
+
+        public enum UIPage
+        {
+            Menu = 0,
+            Comparer,
+            Encoder
+        }
+
+        public Presenter(UIItem[] datas) => this.datas = datas;
+
+        public void ShowUI(UIPage ui)
+        {
+            foreach (var data in datas)
+            {
+                data.ShowElements(data.CorrespondingPage == ui);
+            }
+        }
+    }
+}
